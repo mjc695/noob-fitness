b05bcbcfc18a7368765ed3b341ec088a22a5f016
Monferd Collin <mcollin@Monferds-MacBook-Pro.local>
2018-11-09 16:29:33 -0500

    fixing back end

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
-// const pkg = require('../../package.json')
+const pkg = require('../../package.json');
 
 const databaseName = 'stackathon';
-// pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
